9838a7b9857c3cdd62a3dcb24fdec6534923a88a
Hendrik Araújo Mourão <hendrik.mourao@autotrac.com.br>
2018-07-24 15:31:02 -0300

    import posh-alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,12 @@
 Import-Module posh-git
 Import-Module oh-my-posh
 Import-Module z
+Import-Module posh-alias
+
 Set-Theme agnoster
 
 $env:LC_ALL='C.UTF-8'
+
+# Alias
+Add-Alias ci 'git commit'
+Add-Alias st 'git status'
