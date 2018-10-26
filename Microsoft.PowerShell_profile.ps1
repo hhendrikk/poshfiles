68b8f45bfb288ff1cd28fadc6024358ee3d470b6
Hendrik Araújo Mourão <hendrik.mourao@autotrac.com.br>
2018-10-26 09:11:40 -0300

    Install posh Get-ChildItemColor

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,17 +2,14 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Import-Module z
 Import-Module posh-alias
+Import-Module Get-ChildItemColor
 
-Set-Theme agnoster
+Set-Theme Honukai
 
 $env:LC_ALL='C.UTF-8'
 
-# Alias
-Add-Alias add 'git add'
-Add-Alias ci 'git commit'
-Add-Alias st 'git status'
-Add-Alias lg1 'git lg1'
-Add-Alias lg 'git lg'
-Add-Alias push 'git push'
-Add-Alias pullr 'git pull --rebase'
-Add-Alias pullo 'git pull --ff-only'
+# Config encoding python
+$env:PYTHONIOENCODING="utf-8"
+
+Set-Alias l Get-ChildItemColor -option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
